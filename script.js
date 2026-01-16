9e03c95f043847f1d2cd29c1eef945da0698e13e
Gayatri-Nibedita <nibedita2004s@gmail.com>
2026-01-16 18:33:48 +0530

    Day 2: Parallax, animations, flip cards, horizontal scroll

---
+++ script.js
@@ -0,0 +1,17 @@
+AOS.init({
+    duration: 1000,
+    once: true
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".horizontal-track", {
+    xPercent: -300,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".horizontal",
+        pin: true,
+        scrub: 1,
+        end: "+=2000"
+    }
+});
